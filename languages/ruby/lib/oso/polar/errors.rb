2d53769813b916d94514aa6103f9b0e6eb5a20fe
Sam Scott <sam@osohq.com>
2020-08-23 18:00:25 -0400

    Move file checking into core.
    Reduces the duplication needed, and unlocks handling conflicts
    in the core (e.g. remove all rules whose source matches the
    new filesname).
    
    Made the usage of `load` and `load_str` a little more consistent
    across core + host languages.

--- languages/ruby/lib/oso/polar/errors.rb
+++ languages/ruby/lib/oso/polar/errors.rb
@@ -27,6 +27,7 @@ module Oso
     class UnsupportedError < PolarRuntimeError; end
     class PolarTypeError < PolarRuntimeError; end
     class StackOverflowError < PolarRuntimeError; end
+    class FileLoadingError < PolarRuntimeError; end
 
     # Errors originating from this side of the FFI boundary.
 
@@ -40,25 +41,6 @@ module Oso
     class InlineQueryFailedError < PolarRuntimeError; end
     class NullByteInPolarFileError < PolarRuntimeError; end
     class UnexpectedPolarTypeError < PolarRuntimeError; end
-    class PolarFileAlreadyLoadedError < PolarRuntimeError # rubocop:disable Style/Documentation
-      # @param file [String]
-      def initialize(file)
-        super("File #{file} has already been loaded.")
-      end
-    end
-    class PolarFileContentsChangedError < PolarRuntimeError # rubocop:disable Style/Documentation
-      # @param file [String]
-      def initialize(file)
-        super("A file with the name #{file}, but different contents, has already been loaded.")
-      end
-    end
-    class PolarFileNameChangedError < PolarRuntimeError # rubocop:disable Style/Documentation
-      # @param file [String]
-      # @param existing [String]
-      def initialize(file, existing)
-        super("A file with the same contents as #{file} named #{existing} has already been loaded.")
-      end
-    end
     class PolarFileExtensionError < PolarRuntimeError # rubocop:disable Style/Documentation
       def initialize(file)
         super("Polar files must have .polar extension. Offending file: #{file}")
